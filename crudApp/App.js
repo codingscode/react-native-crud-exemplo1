e04e45f268fadb55cd6fda110d717c1abd1f6d4b
codingscode <heronnestg@hotmail.com>
2023-03-22 12:16:56 -0300

    parte 1

---
+++ crudApp/App.js
@@ -0,0 +1,24 @@
+
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+
+
+function App() {
+  
+   return (
+      <View style={styles.container} >
+         <Text>Testando</Text>
+      </View> 
+   )
+}
+
+const styles = StyleSheet.create({
+   container: {
+     marginTop: 32,
+     paddingHorizontal: 24,
+   },
+
+})
+
+export default App
